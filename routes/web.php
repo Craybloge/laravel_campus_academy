6b239a08e5d6d9f59b4143da7a5db4c5e47f7e90
Mathieu Pithon <35268109+Craybloge@users.noreply.github.com>
2022-08-08 10:50:38 +0200

    tutorial partie 1

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 
 Route::redirect('/', '/home');
 
+
 Route::get('/home', [HomeController::class, 'index']);
 
 Route::get('/about', [AboutController::class, 'index']);
     
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
